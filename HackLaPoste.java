3446861b41bcc98254b6130e375e19f382fd148f
unknown <robert.roussely.lp4@gmail.com>
2019-11-06 10:29:51 +0100

    maj

--- HackLaPoste.java
+++ HackLaPoste.java
@@ -6,39 +6,9 @@ import java.io.PrintWriter;
 import java.io.IOException;
 
 public class HackLaPoste {
-    String path;
-    boolean append_to_file = false;
-/* 
-    public HackLaPoste() {
-       this.path = file_path;
-} */
-/* 
-public HackLaPoste(String file_path, boolean append_value){
-    this.path = file_path;
-    this.append_to_file = append_value;
-}
- */
-//public void userName(String ) {
+ 
+public void lireFichier() throws IOException {
     
-
-
-public void lectureFichiers() {
-    ArrayList<String> fichier = new ArrayList<>();
-    try {
-        Scanner scanner = new Scanner(new File("connexion.log"));
-        while(scanner.hasNext())
-        {
-            String line = scanner.next();
-fichier.add(line);        }
-    } catch (FileNotFoundException e) {
-        e.printStackTrace();
-    }
-    System.out.println(fichier);
 }
-public static void main(String[] args) {
-    HackLaPoste hack = new HackLaPoste();
-    hack.lectureFichiers();
-}
-
 
 }
